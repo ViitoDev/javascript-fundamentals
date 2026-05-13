9f4b2e4ae740031da0a6deb6f31eec84ed3a4c24
João Barros <joaovictorpiresdev@gmail.com>
2026-05-13 08:57:07 -0300

    FEAT: Attempts counter on secret number game

--- principal-project/app.js
+++ principal-project/app.js
@@ -1,20 +1,22 @@
 alert("Welcome to the secret number game!");
 let secretNumber = 29;
+let guess;
+let attempts = 1;
 console.log(secretNumber);
-let guess 
 
 // As long as the guess doesn't match the secret number.
 while (guess != secretNumber) {
 	guess = prompt("Enter a number between 1 and 30.");
-	
+
 	// if your guess equals the secret number
 	if (guess == secretNumber) {
-		alert(`You guessed the secret number! The secret number is ${secretNumber}`);
+		alert(`You guessed the secret number! The secret number is ${secretNumber} com ${attempts}!`);
 	} else {
 		if (secretNumber > guess) {
 			alert(`The secret number is bigger than ${guess}`);
 		} else {
 			alert(`The secret number is less than ${guess}`);
 		}
+		attempts++
 	}
 }
