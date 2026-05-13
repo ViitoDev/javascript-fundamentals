ee2705c204bfd8259a4eddd60f76ec052276edf2
João Barros <joaovictorpiresdev@gmail.com>
2026-05-13 09:22:11 -0300

    FEAT: Adding the reverse counter

--- challenges/counter/app.js
+++ challenges/counter/app.js
@@ -1,6 +1,6 @@
 let count = 1
 
-while (count < 11) {
+while (count != 11) {
     alert(`${count}`)
     count++
 }
---
+++ challenges/reverse-counter/app.js
@@ -0,0 +1,7 @@
+let count = 10;
+
+while (count != 0) {
+    alert(`${count}`)
+    count = count - 1
+    console.log(count)
+}
